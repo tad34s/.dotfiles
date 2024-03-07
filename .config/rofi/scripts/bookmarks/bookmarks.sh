469caf0bc230fb7011afc7f60518d8f38bc5ec73
Tadeáš Báča <bacatade@fit.cvut.cz>
2024-03-07 23:24:19 +0100

    Big updates

--- .config/rofi/scripts/bookmarks/bookmarks.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# cat $(dirname $0)/bookmarks.txt | rofi -dmenu -i -p "" | cut -d\- -f2- | xclip -selection clipboard
-ADDRESS=$(cat $(dirname $0)/bookmarks.txt | rofi -dmenu -i -p "" | cut -d\- -f2-)
-if [ -n "$ADDRESS" ]; then
-    brave -e $ADDRESS
-    i3-msg workspace number 2
-fi
